77fb89280419ca8be3ce44ff1d484b56606d43ed
AnvarKubja <aadukubja@gmail.com>
2026-09-07 14:28:25 +0300

    StartPage and TextPage implementation

--- TARge25Maui/TARge25Maui/MauiProgram.cs
+++ TARge25Maui/TARge25Maui/MauiProgram.cs
@@ -13,6 +13,7 @@ namespace TARge25Maui
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Socafe.otf", "Socafe");
                 });
 
 #if DEBUG
--- TARge25Maui/TARge25Maui/StartPage.xaml.cs
+++ TARge25Maui/TARge25Maui/StartPage.xaml.cs
@@ -3,8 +3,9 @@ namespace TARge25Maui;
 public partial class StartPage : ContentPage
 {
 	VerticalStackLayout vst;
+	ScrollView sv;
 	public List<ContentPage> Lehed = new List<ContentPage>() { new TextPage(), new FigurePage() };
-	public List<string> Lehenimed = new List<string>() { "Testid", "Kujundus" };
+	public List<string> Lehenimed = new List<string>() { "Tekst", "Kujundus" };
 	public StartPage()
 	{
 		vst = new VerticalStackLayout { Padding = 20, Spacing = 20};
@@ -13,19 +14,22 @@ public partial class StartPage : ContentPage
 			Button nupp = new Button
 			{
 				Text = Lehenimed[i],
-				FontSize = 20,
-				BackgroundColor = Colors.LightBlue,
-				TextColor = Colors.White,
+				FontSize = 36,
+				FontFamily="Socafe",
+				BackgroundColor = Colors.LightGray,
+				TextColor = Colors.Black,
 				CornerRadius = 10,
+				HeightRequest = 60,
 				ZIndex = i
 			};
-			nupp.Clicked += (s, a) =>
+			vst.Add(nupp);
+			nupp.Clicked += (sender, e) =>
 			{
 				var valik = Lehed[nupp.ZIndex];
 				Navigation.PushAsync(valik);
 			};
-			vst.Add(nupp);
 		}
-		Content = vst;
+		sv = new ScrollView { Content = vst };
+		Content = sv;
 	}
 }
